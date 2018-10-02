6b5027daac981dcb5d4ab8f024124a78efd7515b
“Simaxa7” <simaxa7@mail.ru>
2018-10-02 18:24:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,81 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+  this.state = String(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.state += " add " + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.state += " subtract " + number;
+    return this ;
   }
 
   multiply(number) {
-    // your implementation
+    this.state += " multiply " + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.state += " devide " + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.state += " pow " + number;
+    return this;
   }
+
+  valueOf() {
+    let myArrData = this.state.split(" ");
+    let result = 0;
+    let i = myArrData.length-1;
+    let len = myArrData.length - 1;
+
+    for (let i = len; i >= 0; i--) {
+      
+      if (myArrData[i] == "pow") {
+        result = Math.pow(myArrData[i-1], myArrData[i+1]);
+        myArrData.splice(i - 1, 3, result);
+      }
+    }
+
+    len = myArrData.length - 1;
+    for (let i = len; i >= 0; i--) {
+      
+      if (myArrData[i] == "multiply") {
+        result = Number(myArrData[i-1]) * Number(myArrData[i+1]);
+        myArrData.splice(i - 1, 3, result);
+      }
+    }
+
+    len = myArrData.length - 1;
+    for (let i = len; i >= 0; i--) {
+      
+      if (myArrData[i] == "devide") {
+        result = myArrData[i-1] / myArrData[i+1];
+        myArrData.splice(i - 1, 3, result);
+        // console.log("+++++++", myArrData);
+      }
+    }
+
+    len = myArrData.length - 1;
+    while(myArrData[1] != undefined) {
+
+      if (myArrData[1] == "add") {
+        result = Number(myArrData[0]) + Number(myArrData[2]);
+        myArrData.splice(0, 3, result);
+      } else {
+        result = Number(myArrData[0]) - Number(myArrData[2]);
+        myArrData.splice(0, 3, result);
+      }
+    }
+
+  return  result;
+  }
+
 }
 
 module.exports = SmartCalculator;
